8bc985043dd4e1e4bff0da3a065f4ff8aebf17ee
Avni Goel <avnigoel98@gmail.com>
2021-03-26 19:22:13 +0530

    promises and promises chaining intro

--- Lec9_Promises/facts/promises.js
+++ Lec9_Promises/facts/promises.js
@@ -2,15 +2,17 @@ const fs = require("fs");
 
 // B gets a pending promise          // A -> initially returns a pending promise
 let pendingPromise = fs.promises.readFile("./f1.txt"); // 100gb => 10 min
-console.log(pendingPromise);
+
+console.log(pendingPromise); // tells the status of promise
 // pendingPromise => Promise object whose state is pending !!!
+
 
 // success callback => scb attached to pending promise
 pendingPromise.then(function(data){
     console.log("Inside then ka callback i.e scb");
-    console.log(data + "");
-    console.log(data);
-    console.log(pendingPromise);
+    console.log(data + ""); //data in the form of string
+    console.log(data); //data in the form of low level data
+    console.log(pendingPromise); // tells the status of promise
 });
 
 
@@ -20,3 +22,4 @@ pendingPromise.catch( function(error){
     console.log(error);
     console.log(pendingPromise);
 });
+
