eaccfa478548382a10603b1df575263ffe866669
Avni Goel <avnigoel98@gmail.com>
2021-05-21 14:16:41 +0530

    excel clone css done

--- Module2_DOM/Lec3_Excel_Clone/activity/init.js
+++ Module2_DOM/Lec3_Excel_Clone/activity/init.js
@@ -3,13 +3,13 @@ function initCells(){
     let cellsContent = "<div class='top-left-cell'></div>";
     cellsContent += "<div class='top-row'>"
     for(let i=0 ; i<26 ; i++){
-        cellsContent += `<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`
+        cellsContent += `<div class='top-row-cell' trid="${i}">${String.fromCharCode(65+i)}</div>`
     }
     cellsContent += "</div>"
 
     cellsContent += "<div class='left-col'>"
     for(let i=0 ; i<100 ; i++){
-        cellsContent += `<div class="left-col-cell">${i+1}</div>`
+        cellsContent += `<div class="left-col-cell" lcid="${i}">${i+1}</div>`
     }
     cellsContent += "</div>"
     cellsContent += "<div class='cells'>"
@@ -45,7 +45,8 @@ function initDB(){
                 childrens:[],
                 parents:[],
                 visited:false,
-                fontStyle : {bold:false , italic:false , underline:false}
+                fontStyle : {bold:false , italic:false , underline:false } ,
+                textAlign : "left"
             }
             row.push(cellObject);
         }
