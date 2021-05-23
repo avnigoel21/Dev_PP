4e6f4d8005a618bf8c0bf1043bd8787db1684569
Avni Goel <avnigoel98@gmail.com>
2021-05-23 12:41:08 +0530

    zoom in captured

--- Module2_DOM/Lec5_PepSnap/activity/script.js
+++ Module2_DOM/Lec5_PepSnap/activity/script.js
@@ -1,10 +1,18 @@
 let videoPlayer = document.querySelector("video");
 let recordButton = document.querySelector("#record-video");
 let photoButton = document.querySelector("#capture-photo");
+let zoomIn = document.querySelector("#in");
+let zoomOut = document.querySelector("#out");
 let recordingState = false;
 let constraints = { video: true };
 let recordedData;
 let mediaRecorder;
+
+
+let maxZoom = 3;
+let minZoom = 1;
+let currZoom = 1;
+
 
 (async function () {
   let mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -34,17 +42,34 @@ let mediaRecorder;
     if (recordingState) {
       // stop the recording
       mediaRecorder.stop();
-      recordButton.innerHTML = "Record";
+      recordButton.querySelector("div").classList.remove("record-animate");
     } else {
       //start the recording
       mediaRecorder.start();
-      recordButton.innerHTML = "Recording";
+      recordButton.querySelector("div").classList.add("record-animate");
     }
     recordingState = !recordingState;
   });
 
   photoButton.addEventListener("click", capturePhotos);
+
+  zoomIn.addEventListener("click" , function(){
+    if(currZoom + 0.1 <= maxZoom){
+      currZoom += 0.1;
+      videoPlayer.style.transform = `scale(${currZoom})`;
+    }
+  });
+  zoomOut.addEventListener("click" , function(){
+    if(currZoom - 0.1 >= minZoom){
+      currZoom -= 0.1;
+      videoPlayer.style.transform = `scale(${currZoom})`;
+    }
+  });
 })();
+
+
+
+
 
 function saveVideoToFs() {
   console.log("Saving Video");
@@ -61,14 +86,28 @@ function saveVideoToFs() {
 }
 
 function capturePhotos() {
+  photoButton.querySelector("div").classList.add("capture-animate");
+
+  // async
+  setTimeout(function(){
+    photoButton.querySelector("div").classList.remove("capture-animate");
+  } , 1000);
+
   let canvas = document.createElement("canvas");
   canvas.height = videoPlayer.videoHeight;
   canvas.width = videoPlayer.videoWidth;
-
   let ctx = canvas.getContext("2d");
-  ctx.drawImage(videoPlayer, 0, 0);
 
+  // canvas is scaled according to currZoom
+  if(currZoom != 1){
+    ctx.translate(canvas.width/2 , canvas.height/2);
+    ctx.scale(currZoom , currZoom);
+    ctx.translate(-canvas.width/2 , -canvas.height/2)
+  }
+
+  ctx.drawImage(videoPlayer, 0, 0);
   let imageUrl = canvas.toDataURL("image/jpg"); //canvas object => file url String
+
 
   let aTag = document.createElement("a");
   aTag.download = "photo.jpg";
