9bcb593b955cee735dc0c72e838e7b3552dce387
Avni Goel <avnigoel98@gmail.com>
2021-06-23 08:43:44 -0700

    todo changes done

--- Module3_React/todo/src/App.js
+++ Module3_React/todo/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import InputBox from "./components/InputBox/InputBox.jsx";
+import TodosList from "./components/TodosList/TodosList.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { id: "1", todo: "Learn JSX" },
+      { id: "2", todo: "Learn Css" },
+      { id: "3", todo: "Learn ES6" },
+      { id: "4", todo: "Learn React" },
+      { id: "5", todo: "Learn JS" },
+    ],
+  };
+
+  deleteTodo = (id) => {
+    let updatedTodos = this.state.todos.filter(function (todoObj) {
+      if (todoObj.id == id) {
+        return false;
+      }
+      return true;
+    });
+    // console.log(updatedTodos);
+    this.setState({
+      todos: updatedTodos,
+    });
+  };
+
+  addTodo = (todo) => {
+    let updatedTodos = [
+      ...this.state.todos,
+      { id: this.state.todos.length + 1, todo: todo },
+    ];
+    this.setState({
+      todos:updatedTodos
+    })
+  };
+
+  render() {
+    let todos = this.state.todos;
+    let deleteTodo = this.deleteTodo;
+    let addTodo = this.addTodo;
+    return (
+      <div className="App">
+        <InputBox addTodo={addTodo} ></InputBox>
+        <TodosList todos={todos} deleteTodo={deleteTodo}></TodosList>
+      </div>
+    );
+  }
 }
 
 export default App;
